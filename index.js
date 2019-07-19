ed52d67cf99f3c6d2c33ee1518c69cffdf57c301
blindproj <carlos_zimmerle@yahoo.com.br>
2019-07-19 18:19:08 -0300

    new design

--- index.js
+++ index.js
@@ -2,29 +2,21 @@ const R = require('ramda');
 const latinSquare = require('latin-square');
 const shuffle = require('shuffle-array');
 
-let greek_treatments = ['\u03B1', '\u03B2'];
-let blocking_var1 = ['D1', 'D2'];
-let blocking_var2 = ['I1', 'I2'];
-let blocking_var3 = ['A1', 'B1'];
-let blocking_var4 = ['A2', 'B2'];
+let blocking_vars = ['Device 1', 'Device 2'];
 
-let gTSampler = latinSquare(greek_treatments);
-let bV3Sampler = latinSquare(blocking_var3);
-let bV4Sampler = latinSquare(blocking_var4);
+let treatments  = ['Treatment A', 'Treatment B'];
+let treatSampler = latinSquare(treatments);
 
-console.log('\t     D1\t\t\t  D2');
-greek_treatments.forEach((val, index)=>{
-    console.log(blocking_var2[index], R.transpose([gTSampler(), bV3Sampler(), bV4Sampler()]));
+console.log('    Device 1\t    Device 2');
+treatments.forEach(()=>{
+    console.log(treatSampler());
 });
 
 console.log('\nwhith:');
 
 [
-[shuffle(greek_treatments), shuffle(['RxJS', 'Most.js'])],
-[shuffle(blocking_var1), shuffle(['J7', 'S8'])],
-[shuffle(blocking_var2), shuffle(['minInterval', 'maxInterval'])],
-[shuffle(blocking_var3), shuffle(['tumblingCountWindow', 'slidingCountWindow'])],
-[shuffle(blocking_var4), shuffle(['minWindowSize', 'maxWindowsize'])]
+[shuffle(treatments), shuffle(['RxJS', 'Most.js'])],
+[shuffle(blocking_vars), shuffle(['J7', 'G5 Plus'])]
 ].forEach((val) =>{
     R.transpose(val).forEach((transposed)=>{
         console.log(transposed[0] + ' = ' + transposed[1]);
